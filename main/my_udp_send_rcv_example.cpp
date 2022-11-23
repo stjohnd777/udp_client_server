cbdb419260c6ae48ae346a8d8876a674f0c1efbb
Daniel St. John <stjohnd777@gmail.com>
2022-11-23 12:03:35 -0700

    sync with remote:Wq

--- main/my_udp_send_rcv_example.cpp
+++ main/my_udp_send_rcv_example.cpp
@@ -19,81 +19,74 @@ int main() {
     std::string host = "127.0.0.1";
     unsigned short port = 7767;
 
-
     try {
+
         // client sending thread
-        int send_count = 0;
-        std::thread t([&]() {
-            UdpUtilsSync udpUtil;
-            while (isRunning) {
-                stringstream ss;
-                ss << "Sending Some Data " << send_count << endl;
-                string data = ss.str();
-                try {
-                    udpUtil.ClientSendAndForget(host, port, data);
-                    send_count++;
-                    std::this_thread::sleep_for(1000ms);
+        auto client = [&]() {
+            int send_count = 0;
+            auto t = new std::thread([&]() {
+                UdpUtilsSync udpUtil;
+                while (isRunning) {
+                    stringstream ss;
+                    ss << "Sending Some Data " << send_count << endl;
+                    string data = ss.str();
+                    try {
+                        udpUtil.ClientSendAndForget(host, port, data);
+                        send_count++;
+                        cout << send_count << ": ClientSendAndForget:" << data << endl;
+                        std::this_thread::sleep_for(1000ms);
+                    }
+                    catch (std::exception &ex) {
+                        std::cerr << ex.what() << endl;
+                        throw ex;
+                    }
                 }
-                catch (std::exception &ex) {
-                    std::cerr << ex.what() << endl;
-                    throw ex;
-                }
-            }
-            std::cout << "Sending Thread exiting" << std::endl;
-        });
-
-
+                std::cout << "Sending Thread exiting" << std::endl;
+            });
+            return t;
+        };
+        client()->detach();
 
 
         // receiving thread, we are acting like a server
         boost::circular_buffer<std::string> circular_buffer(3);
 
-        // Works
-        //        std::thread q([&]() {
-        //            boost::asio::io_service io_context;
-        //            boost::asio::ip::udp::socket socket(io_context, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), 7767));
-        //            while (isRunning) {
-        //                boost::array<char, 24> recv_buf;
-        //                boost::asio::ip::udp::endpoint remote_endpoint;
-        //                std::size_t bytes_received = socket.receive_from(boost::asio::buffer(recv_buf), remote_endpoint);
-        //                string data(recv_buf.begin(), bytes_received);
-        //                circular_buffer.push_back(data);
-        //            }
-        //        });
-
-
-//        auto server = [&] (){
-        std::thread q([&]() {
-            UdpUtilsSync udpUtil;
-            while (isRunning) {
-                try {
-                    auto data = udpUtil.ServerReceiveAndForget(host, port);
-                    circular_buffer.push_back(data);
+        auto server = [&]() {
+            auto t = new std::thread([&]() {
+                UdpUtilsSync udpUtil;
+                int recv_count = 0;
+                while (isRunning) {
+                    try {
+                        auto data = udpUtil.ServerReceiveAndForget(host, port);
+                        cout << recv_count << ": ServerReceiveAndForget:" << data << endl;
+                        recv_count++;
+                        circular_buffer.push_back(data);
+                    }
+                    catch (std::exception &ex) {
+                        std::cerr << ex.what() << endl;
+                        throw ex;
+                    }
                 }
-                catch (std::exception &ex) {
-                    std::cerr << ex.what() << endl;
-                    throw ex;
-                }
-            }
-            std::cout << "Receiving Thread exiting" << std::endl;
-        });
-//        };
-//        server();
-
+                std::cout << "Receiving Thread exiting" << std::endl;
+            });
+            return t;
+        };
+        server()->detach();
 
 
         std::cout << "Main thread pause for 30s" << std::endl;
         std::this_thread::sleep_for(30 * 1000ms);
         isRunning = false;
-        std::this_thread::sleep_for(1000ms);
+        std::this_thread::sleep_for(2000ms);
 
         // take look at the circular buffer
 
         int index = 0;
         for_each(begin(circular_buffer), end(circular_buffer), [&](std::string m) {
             std::cout << "index:" << index << "data:" << m << std::endl;
-            index ++;
+            index++;
         });
+
 
         std::cout << "Main thread falling off the end of it brace" << std::endl;
 
